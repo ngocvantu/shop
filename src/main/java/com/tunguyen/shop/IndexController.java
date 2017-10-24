11c805d1ba12233085acdb99e99c9e2f904e56be
ngocvantu <tunguyen4078@gmail.com>
2017-10-24 14:40:40 +0700

    33333333

--- src/main/java/com/tunguyen/shop/IndexController.java
+++ src/main/java/com/tunguyen/shop/IndexController.java
@@ -1,13 +1,15 @@
 package com.tunguyen.shop;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 	
 	@GetMapping("/")
-	public String index(){
+	public String index(Model model){
+		model.addAttribute("currentMenu", "anhviet");
 		return "index";
 	}
 }

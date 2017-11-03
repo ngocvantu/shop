a8dbaa311379639ce191aa1e007f38d1b939f247
ngocvantu <tunguyen4078@gmail.com>
2017-11-03 15:45:28 +0700

    add hibernate mysql support

--- src/main/java/com/tunguyen/shop/IndexController.java
+++ src/main/java/com/tunguyen/shop/IndexController.java
@@ -1,15 +1,30 @@
 package com.tunguyen.shop;
 
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Service
+@Transactional
 public class IndexController {
+	
+	@Autowired
+	SessionFactory sessionFactory;
 	
 	@GetMapping("/")
 	public String index(Model model){
 		model.addAttribute("currentMenu", "anhviet");
+		
+		Session session;
+		session = sessionFactory.getCurrentSession();
 		
 		return "index";
 	}
